4bf1346b3faba75eb7737a94592bb083e70493a5
ypxu <nelson0235@gmail.com>
2014-03-05 21:28:16 -0800

    add link_dirs into deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :deploy_via, :remote_cache
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
