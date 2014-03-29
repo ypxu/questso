b96a594af449bb08a0f05c93e0e7b66aa3875dd2
ypxu <nelson0235@gmail.com>
2014-03-28 19:09:55 -0700

    fix application name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'trenvenue'
+set :application, 'questso'
 set :repo_url, 'git@github.com:ypxu/questso.git'
 set :user, "questso"
 set :use_sudo, false
